5abf2cc09264c3dc5a8763481c93c13a17b69e50
Bruno Santos <bruno.js02@gmail.com>
2020-05-20 17:27:22 -0300

    Updated startup function to connect to mongo db

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { json } from "body-parser";
 import "express-async-errors";
+import mongoose from "mongoose";
 
 import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
@@ -23,6 +24,21 @@ app.all("*", async () => {
 
 app.use(errorHanlder);
 
-app.listen(3000, () => {
-  console.log("Listening on 3000");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Connected to mongo db");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on 3000");
+  });
+};
+
+start();
