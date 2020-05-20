8c0bf20b3320844425dc4a1dc3e045ea1482c1d4
Bruno Santos <bruno.js02@gmail.com>
2020-05-20 16:22:59 -0300

    Updated to use request validation error class

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
 
 const router = express.Router();
 
@@ -14,8 +15,8 @@ router.post(
   ],
   (req: Request, res: Response) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty) {
-      throw new Error("Invalid email or password");
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
     }
     res.send({});
   }
