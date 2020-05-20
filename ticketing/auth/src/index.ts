ea18ea1ac7a22d7458d44da6e6277406f28e25e6
Bruno Santos <bruno.js02@gmail.com>
2020-05-20 13:45:28 -0300

    Updated index to use error middleware and use routers

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import { json } from "body-parser";
+import { signinRouter } from "./routes/signin";
+import { signupRouter } from "./routes/signup";
+import { signoutRouter } from "./routes/signout";
+import { currentUserRouter } from "./routes/current-user";
+import { errorHanlder } from "./middlewares/erro-handler";
 
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (_, res) => {
-  res.send("Hi there");
-});
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+app.use(currentUserRouter);
+
+app.use(errorHanlder);
 
 app.listen(3000, () => {
   console.log("Listening on 3000");
