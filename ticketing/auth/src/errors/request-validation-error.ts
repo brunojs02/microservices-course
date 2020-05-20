1529d02a06923cf917a977e6e07ee2ede2b2c995
Bruno Santos <bruno.js02@gmail.com>
2020-05-20 16:22:15 -0300

    Created request validation error class

--- ticketing/auth/src/errors/request-validation-error.ts
+++ ticketing/auth/src/errors/request-validation-error.ts
@@ -1,7 +1,18 @@
 import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
 
-export class RequestValidationError extends Error {
-  constructor(private errors: ValidationError[]) {
-    super();
+export class RequestValidationError extends CustomError {
+  statusCode: number = 400;
+
+  constructor(private _errors: ValidationError[]) {
+    super("Invalid request params");
+    Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this._errors.map(({ msg: message, param: field }) => ({
+      field,
+      message,
+    }));
   }
 }
