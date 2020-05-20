51abae361215aa3c1026407ae00ef5aaef6eb7ff
Bruno Santos <bruno.js02@gmail.com>
2020-05-20 16:23:19 -0300

    Added config to get all route

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { json } from "body-parser";
+import "express-async-errors";
+
 import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
 import { currentUserRouter } from "./routes/current-user";
-import { errorHanlder } from "./middlewares/erro-handler";
+import { errorHanlder } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -13,6 +16,10 @@ app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
 app.use(currentUserRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
 
 app.use(errorHanlder);
 
