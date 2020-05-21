f60dde885fc3f30a7b58b72c9ce50314ae646ffc
Bruno Santos <bruno.js02@gmail.com>
2020-05-21 18:13:00 -0300

    Added validation to verify if JWT_KEY is in env

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { json } from "body-parser";
 import "express-async-errors";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
@@ -11,7 +12,9 @@ import { errorHanlder } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
+app.use(cookieSession({ signed: false, secure: true }));
 
 app.use(signinRouter);
 app.use(signupRouter);
@@ -25,6 +28,10 @@ app.all("*", async () => {
 app.use(errorHanlder);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
       useNewUrlParser: true,
