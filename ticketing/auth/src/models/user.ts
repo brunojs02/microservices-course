6e24d4b31a13d58ad023348e95a4046c72ae0ed8
Bruno Santos <bruno.js02@gmail.com>
2020-05-21 15:48:35 -0300

    Updated User model to hash password before save to database

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import { model, Model, Schema, Document } from "mongoose";
+import { Password } from "../services/password";
 
 interface UserAttrs {
   email: string;
@@ -23,6 +24,14 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
