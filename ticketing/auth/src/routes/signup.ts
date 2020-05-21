da5c56738a27bd76f3aabe188e7aaaf402a3b799
Bruno Santos <bruno.js02@gmail.com>
2020-05-21 18:13:35 -0300

    Updated signup controller to create a session with jwt token

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 import { User } from "../models/user";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
@@ -31,6 +32,13 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    const userJwt = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_KEY!
+    );
+
+    req.session = { jwt: userJwt };
 
     res.status(201).send(user);
   }
