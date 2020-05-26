e891ed2645b0d6a14e7399b7f5e660cb093e9801
Bruno Santos <bruno.js02@gmail.com>
2020-05-26 17:39:20 -0300

    Created test for signout route

---
+++ ticketing/auth/src/routes/__test__/signout.test.ts
@@ -0,0 +1,19 @@
+import request from "supertest";
+
+import { app } from "../../app";
+
+it("clear the cookies after sign out", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "bruno@example.com", password: "1234" })
+    .expect(201);
+
+  const response = await request(app)
+    .post("/api/users/signout")
+    .send({})
+    .expect(200);
+
+  expect(response.get("Set-Cookie")[0]).toEqual(
+    "express:sess=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly"
+  );
+});
