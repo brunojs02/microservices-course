a1146e3700fb1382e7469e7cce757bc7b9791ae8
Bruno Santos <bruno.js02@gmail.com>
2020-11-20 17:07:20 -0300

    Added console.error to see the errors that aren't handled

--- ticketing/common/src/middlewares/error-handler.ts
+++ ticketing/common/src/middlewares/error-handler.ts
@@ -11,5 +11,6 @@ export const errorHanlder = (
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
+  console.error(err);
   res.status(400).send({ errors: [{ message: "Something went wrong" }] });
 };
