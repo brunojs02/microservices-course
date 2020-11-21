4d61b763e8a1e9fcf621d2cdd4007e757e7303b0
Bruno Santos <bruno.js02@gmail.com>
2020-11-21 19:38:08 -0300

    fixed ticket update event interface

--- ticketing/common/src/events/ticket-update-event.ts
+++ ticketing/common/src/events/ticket-update-event.ts
@@ -1,6 +1,6 @@
 import { Subjects } from "./subjects";
 
-export interface TicketCreatedEvent {
+export interface TicketUpdatedEvent {
   subject: Subjects.TicketUpdated;
   data: {
     id: string;
