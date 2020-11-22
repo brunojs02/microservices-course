b9fbd16545b1afaf10358bbb1fa71f859e520fb3
Bruno Santos <bruno.js02@gmail.com>
2020-11-22 17:29:45 -0300

    added logic to create a ticket

--- ticketing/orders/src/routes/new.ts
+++ ticketing/orders/src/routes/new.ts
@@ -4,16 +4,46 @@ import {
   requireAuth,
   requestValidation,
 } from "@mscticketing/common/build/middlewares";
+import {
+  NotFoundError,
+  BadRequestError,
+} from "@mscticketing/common/build/errors";
+import { Ticket } from "../models/ticket";
+import { Order, OrderStatus } from "../models/order";
 
 const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post(
   "/api/orders",
   requireAuth,
   [body("ticketId").not().isEmpty().withMessage("ticketId must be provided")],
   requestValidation,
-  async (_: Request, res: Response) => {
-    res.sendStatus(200);
+  async (req: Request, res: Response) => {
+    const { body, currentUser } = req;
+    const { id: userId } = currentUser!;
+    const { ticketId } = body;
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket) throw new NotFoundError();
+
+    const isReserved = await ticket.isReserved();
+
+    if (isReserved) throw new BadRequestError("Ticket is already reserved");
+
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    const order = Order.build({
+      userId,
+      ticket,
+      expiresAt: expiration,
+      status: OrderStatus.Created,
+    });
+    await order.save();
+
+    res.status(201).send(order);
   }
 );
 
