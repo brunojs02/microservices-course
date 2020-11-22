ea09300b146424ad1f9bccea3506f8f51b3939a3
Bruno Santos <bruno.js02@gmail.com>
2020-11-22 19:18:35 -0300

    implemented show specific order router

--- ticketing/orders/src/routes/show.ts
+++ ticketing/orders/src/routes/show.ts
@@ -1,9 +1,27 @@
 import express, { Request, Response } from "express";
+import {
+  NotFoundError,
+  NotAuthorizedError,
+} from "@mscticketing/common/build/errors";
+import { requireAuth } from "@mscticketing/common/build/middlewares";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.get("/api/orders/:id", async (_: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.get(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { params, currentUser } = req;
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId).populate("ticket");
+
+    if (!order) throw new NotFoundError();
+
+    if (order.userId !== currentUser!.id) throw new NotAuthorizedError();
+
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
