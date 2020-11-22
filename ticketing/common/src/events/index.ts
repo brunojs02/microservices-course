efd06de0894b8633c93bf67dbefbb5f25a278f59
Bruno Santos <bruno.js02@gmail.com>
2020-11-22 16:35:53 -0300

    created order status enum

--- ticketing/common/src/events/index.ts
+++ ticketing/common/src/events/index.ts
@@ -1,5 +1,6 @@
 export * from "./subjects";
 export * from "./base-listener";
 export * from "./base-publisher";
+export * from "./types/order-status";
 export * from "./ticket-create-event";
 export * from "./ticket-update-event";
